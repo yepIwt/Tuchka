a1e1e3951f80c8c87a813be32c9d80e39bf239c8
yepiwt <n.serg1evsky@gmail.com>
2021-01-06 01:33:32 +0300

    Добавил шифрование DES

--- confs/__init__.py
+++ confs/__init__.py
@@ -1,3 +1,4 @@
+import os
 from vk_api import VkApi
 from Crypto.Cipher import DES
 
@@ -7,8 +8,11 @@ PEER_CONST = 2000000000
 class Config(object):
     
     def __init__(self):
-        # todo: проверка config.cfg
-        pass
+        if os.path.exists('data'):
+            config_as_str = self.decrypt(input('Введите пароль: '))
+            print(config_as_str)
+        else:
+            self.new_cfg()
     
     def get_all_archives(self,token: str) -> list:
         session = VkApi(token=token)
@@ -21,33 +25,37 @@ class Config(object):
                     all_archives.append({'name':'todo','id':d['peer_id']})
         return all_archives
     
-    def pad(self,text: bytes) -> bytes:
+    def pad(self,text):
         while len(text) % 8 != 0:
             text += b' '
         return text
 
-    def encrypt(self,key: str, data: str) -> bytes:
-        des = DES.new(key, DES.MODE_ECB)
-        encrypted_text = des.encrypt(data.encode('utf-8'))
-        print("encrypted: {}".format(encrypted_text))
-        self.decrypt(key,encrypted_text)
-        #return encrypted_text
-    
-    def decrypt(self,key: str, data: bytes) -> str:
-        des = DES.new(key, DES.MODE_ECB)
-        #padded_test = bytes(data, encoding = 'utf-8')
-        decrypted = des.decrypt(data)
-        print("decrypted: {}",format(decrypted))
+    def encrypt(self,key: str, data: str) -> None:
+       des = DES.new(key.encode('utf-8'), DES.MODE_ECB)
+       padded_text = self.pad(data.encode('utf-8'))
+       encrypted = des.encrypt(padded_text)
+       with open('data','wb') as f:
+           f.write(encrypted)
+
+    def decrypt(self,key: str) -> str:
+        with open('data','rb') as f:
+            data = f.read()
+        des = DES.new(key.encode('utf-8'), DES.MODE_ECB)
+        try:
+            decrypted = des.decrypt(data).strip().decode('utf-8')
+            return decrypted
+        except UnicodeDecodeError:
+            print('Неправильный пароль!')
+            exit()
 
     def new_cfg(self):
-        token = input('Введите токен для vk_api (kate mobile token)')
+        token = input('Введите токен для vk_api (kate mobile token): ')
         archives = self.get_all_archives(token)
         new_config = {
             'token': token, 
             'sync_chat':None,
             'archives': archives,
         }
-        print(new_config)
+        self.encrypt(input('Введите новый пароль: '),str(new_config))
 
 a = Config()
-a.encrypt('12345678',"infoinfo")
