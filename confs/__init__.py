779137c4474e4a882cbfbc206749f841a84290ce
yepiwt <n.serg1evsky@gmail.com>
2021-01-07 15:38:56 +0300

    Закончил делать конфиги

--- confs/__init__.py
+++ confs/__init__.py
@@ -1,4 +1,6 @@
 import os
+import ast
+import crypt
 from vk_api import VkApi
 from Crypto.Cipher import DES
 
@@ -6,13 +8,15 @@ SYNC_CODE = "Archive: RatherCloudy"
 PEER_CONST = 2000000000
 
 class Config(object):
-    
-    def __init__(self):
-        if os.path.exists('data'):
-            config_as_str = self.decrypt(input('Введите пароль: '))
-            print(config_as_str)
-        else:
+
+    __slots__ = ('crypter','data')
+
+    def __init__(self,passw):
+        self.crypter = crypt.LetItCrypt(passw)
+        if not os.path.exists('data'):
             self.new_cfg()
+        config_as_str = self.crypter.dec()
+        self.data = ast.literal_eval(config_as_str)
     
     def get_all_archives(self,token: str) -> list:
         session = VkApi(token=token)
@@ -24,29 +28,6 @@ class Config(object):
                 if d['peer_id'] > PEER_CONST:
                     all_archives.append({'name':'todo','id':d['peer_id']})
         return all_archives
-    
-    def pad(self,text):
-        while len(text) % 8 != 0:
-            text += b' '
-        return text
-
-    def encrypt(self,key: str, data: str) -> None:
-       des = DES.new(key.encode('utf-8'), DES.MODE_ECB)
-       padded_text = self.pad(data.encode('utf-8'))
-       encrypted = des.encrypt(padded_text)
-       with open('data','wb') as f:
-           f.write(encrypted)
-
-    def decrypt(self,key: str) -> str:
-        with open('data','rb') as f:
-            data = f.read()
-        des = DES.new(key.encode('utf-8'), DES.MODE_ECB)
-        try:
-            decrypted = des.decrypt(data).strip().decode('utf-8')
-            return decrypted
-        except UnicodeDecodeError:
-            print('Неправильный пароль!')
-            exit()
 
     def new_cfg(self):
         token = input('Введите токен для vk_api (kate mobile token): ')
@@ -56,6 +37,4 @@ class Config(object):
             'sync_chat':None,
             'archives': archives,
         }
-        self.encrypt(input('Введите новый пароль: '),str(new_config))
-
-a = Config()
+        self.crypter.enc(str(new_config))
