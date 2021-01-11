326e1cae32615679ee9fe42880fa842a233cc1f2
yepiwt <n.serg1evsky@gmail.com>
2021-01-12 01:10:23 +0300

    Добавил очистку раздела "Файлы"

--- bot.py
+++ bot.py
@@ -11,10 +11,9 @@ class Base(object):
         self.config = confs.Config('password')
         if not self.has_sync_chat():
             self.new_sync_chat()
-        #print('Syncing remote files...')
-        #self.config.data['sync_files'] = self.get_attachments_from(self.config.data['sync_chat'])
-        #print('Remote files synced')
-        self.upload_file_to_archive('.gitignore')
+        print('Syncing remote files...')
+        self.config.data['sync_files'] = self.get_attachments_from(self.config.data['sync_chat'])
+        print('Remote files synced')
         self.save()
     
     def get_attachments_from(self, archive_id: int):
@@ -43,11 +42,16 @@ class Base(object):
         new_uploaded_file = up.document(doc = path_to_file,title=filename)
         return new_uploaded_file['doc']['owner_id'], new_uploaded_file['doc']['id']
     
+    def delete_temp_file(self, owner_id: str, file_id: str):
+        self.config.api.docs.delete(owner_id=owner_id, doc_id=file_id)
+
     def upload_file_to_archive(self,path_to_file: str, archive_id = None):
         if not archive_id:
             owner_id, file_id = self.upload_file(path_to_file)
             attach = 'doc' + str(owner_id) + '_' + str(file_id)
             self.config.api.messages.send(peer_id = self.config.data['sync_chat'], attachment=attach, random_id = 0)
+        self.delete_temp_file(owner_id,file_id)
+            
 
     def save(self):
         self.config.save_in_file()
