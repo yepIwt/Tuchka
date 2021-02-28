7d9885b00f24d9cca66d7ebefc187bec44b3deed
yepiwt <n.serg1evsky@gmail.com>
2021-02-28 14:18:15 +0300

    add unzipDir.py

--- hideFolder.py
+++ hideFolder.py
@@ -8,8 +8,12 @@ def zipdir(path, ziph):
     for root, dirs, files in os.walk(path):
         for file in files:
             ziph.write(os.path.join(root, file), os.path.relpath(os.path.join(root, file), os.path.join(path, '..')))
-  
+
+def unzipdir(file: str):
+    with zipfile.ZipFile(file, 'r') as file:
+        file.extractall('.')
 #if __name__ == '__main__':
 #    zipf = zipfile.ZipFile('file.zip', 'w', zipfile.ZIP_DEFLATED)
 #    zipdir('secret', zipf)
 #    zipf.close()
+
