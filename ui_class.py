ed09a13a1a1fb3f4fd568f7ab28ac55bdad1416b
Sergievsky Nikita <reetray@protonmail.com>
2022-03-20 18:47:38 +0300

    Feat: Add start MainWindow

--- ui_class.py
+++ ui_class.py
@@ -16,10 +16,36 @@ class Registration(QtWidgets.QMainWindow):
 		self.credentials = (log, passw)
 		self.close()
 
+class ArchiveView(QtWidgets.QWidget):
+	
+	def __init__(self):
+		super(ArchiveView, self).__init__()
+		uic.loadUi('ui/ArchiveView.ui', self)
+		self.UpButton.clicked.connect(self.go_up)
+		self.DownButton.clicked.connect(self.go_down)
+		self.SelectButton.clicked.connect(self.go_change_release)
+	
+	def go_up(self):
+		print("Go Up!")
+
+	def go_down(self):
+		print("Go Down!")
+	
+	def go_change_release(self):
+		print("Change Release to ... !")
+
+# QStackedWidget changes pages to (ArchiveView, ...)
+class MainWindow(QtWidgets.QMainWindow):
+
+	def __init__(self):
+		super(MainWindow, self).__init__()
+		uic.loadUi('ui/MainWindow.ui', self)
+		sel_ach = ArchiveView()
+		self.pages.addWidget(sel_ach)
+
 
 if __name__ == "__main__":
 	app = QtWidgets.QApplication(sys.argv)
-	ui = Registration()
+	ui = MainWindow()
 	ui.show()
 	app.exec_()
-	print(ui.credentials)
