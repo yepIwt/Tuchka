d6e377ab2b2c77c311ce26ae98191ffbb2c4f154
Sergievsky Nikita <ReetRay@protonmail.com>
2023-02-27 10:06:13 +0300

    Headers: Добавлен класс Locker

--- ui_header.py
+++ ui_header.py
@@ -1,7 +1,34 @@
 
-
+import confs
 from PyQt5 import QtCore, QtWidgets, uic, QtGui
 
+
+class Locker(QtWidgets.QMainWidow):
+	
+
+	def __init__(self, config: confs.Config):
+
+		# Init frontend
+		super(Locker, self).__init__()
+		uic.loadUi("screens/Locker.ui", self)
+
+		# Init backend
+		self.ExecuteButton.clicked.connect(self.__unlock)
+		self.ConfigPassword.setEchoMode(QtWidgets.QLineEdit.Password)  # Активация по Enter
+		self._cfg = config
+	
+	def __unlock(self):
+		
+		result = self._cfg.open(self.ConfigPassword.text())
+
+		if result:
+			self.close()
+		else:
+			self.locked()
+	
+	def locked(self):
+		self.ConfigPassword.setText("")
+		self.log_msg.setText("Пароль не совпадает")
 
 
 class AppRegistrationMenu(QtWidgets.QMainWindow):
